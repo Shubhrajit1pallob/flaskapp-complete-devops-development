8a40774558838c5b5e35ecf221a9eff4786600d4
Shubhrajit1pallob <spallob@asu.edu>
2025-07-19 20:03:06 -0700

    updated the outputs file.

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -12,3 +12,8 @@ output "innstance_public_ip" {
   value       = aws_instance.flask_app_instance.public_ip
   description = "Public IP address of the Flask app EC2 instance"
 }
+
+output "instance_public_id" {
+  value = aws_instance.flask_app_instance.id
+  description = "ID of the Flask app EC2 instance"
+}
